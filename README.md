4cd66ce8b639039e2b93157bb5dd1e9c55ec9288
Luis Caceres <lccaceres@outlook.com>
2023-12-12 12:25:59 -0600

    Feat: Add seed data in database and get all Pokémon by pagination

--- README.md
+++ README.md
@@ -20,6 +20,11 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
+5. Insertar datos de prueba
+```
+http://localhost:3000/api/v2/seed
+```
+
 ## Stack usado
 * MongoDb
 * Nest
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,6 +4,7 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
 import { Model, isValidObjectId } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from '../common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -20,8 +21,11 @@ export class PokemonService {
     }
   }
 
-  async findAll() {
-    return await this.pokemonModel.findOne()
+  async findAll(paginationDto: PaginationDto) {
+
+    const { limit = 10, offset = 0} = paginationDto
+
+    return await this.pokemonModel.find().limit(limit).skip(offset).sort({ no: 1 }).select('-__v')
   }
 
   async findOne(term: string) {
